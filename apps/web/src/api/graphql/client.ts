e12e3fc3907bb234703d8b4207bd731e8b3b4c60
EWOKWICKET <ihushchin@gmail.com>
2026-04-02 23:48:02 +0300

    fix lint

--- apps/web/src/api/graphql/client.ts
+++ apps/web/src/api/graphql/client.ts
@@ -1,6 +1,3 @@
 import { GraphQLClient } from 'graphql-request';
 
-export const gqlClient = new GraphQLClient(
-  import.meta.env.VITE_GRAPHQL_URL ?? `${location.origin}/graphql`,
-  { credentials: 'include' },
-);
+export const gqlClient = new GraphQLClient(import.meta.env.VITE_GRAPHQL_URL ?? `${location.origin}/graphql`, { credentials: 'include' });
